d531338e9cd51a7152318043e1ee5ab9d2c8fc38
orion253 <lr921@ic.ac.uk>
2023-11-28 09:29:19 +0000

    setup frame and made patient class

--- SIsystem/src/main/java/Main.java
+++ SIsystem/src/main/java/Main.java
@@ -1,4 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Main {
     public static void main(String[] args) {
+        // setting up my GUI frame
+        JFrame frame = new JFrame("Stroke investigation system");
+        frame.setSize(450,400);
+
+        JPanel displayPanel = new JPanel();
+
+        frame.setContentPane(displayPanel);
+        frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {// Ends program if close window is clicked
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+
     }
 }
---
+++ SIsystem/src/main/java/Patient.java
@@ -0,0 +1,13 @@
+public class Patient {
+    //fields
+    String name;
+    int age;
+
+    //constructor
+    public Patient(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //methods
+}
